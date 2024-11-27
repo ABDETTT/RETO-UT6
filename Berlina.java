23121552d68934eda186e722ad527c9acfef0155
Abdelilah Hajji <abdehajj80@gmail.com>
2024-11-27 17:45:53 +0100

    Creacion de la clase Deportivo y Berlina

---
+++ Berlina.java
@@ -0,0 +1,20 @@
+public class Berlina {
+    
+    private String marca;
+    private String motor;
+    private String color;
+    private boolean tarccionCuatro;
+
+    public Berlina(String marca, String motor, String color, boolean tarccionCuatro) {
+
+        this.marca = marca;
+        this.motor = motor;
+        this.color = color;
+        this.tarccionCuatro = tarccionCuatro;
+
+    }
+
+    
+
+    
+}
---
+++ Deportivo.java
@@ -0,0 +1,19 @@
+public class Deportivo {
+    
+    private String marca;
+    private String motor;
+    private String color;
+    private boolean tarccionCuatro;
+
+    public Deportivo(String marca, String motor, String color, boolean tarccionCuatro) {
+        this.marca = marca;
+        this.motor = motor;
+        this.color = color;
+        this.tarccionCuatro = tarccionCuatro;
+    }
+
+    
+    
+    
+
+}
