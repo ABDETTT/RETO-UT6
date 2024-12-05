98ab1df4b4d6d51b2ab1b7c8532afd4080350437
Amine55ee <marruecosrabat4@gmail.com>
2024-12-05 20:50:47 +0100

    Actualizado todooooo

--- CocheMain.java
+++ CocheMain.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class CocheMain {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
 
         Cliente cliente1 = new Cliente("abde", 22, "4170sfs34", "Hajji", "Hajji", "Calle donado");
     }
 
-    public void verMenu(Scanner sc){
+    public void verMenu(Scanner sc) {
         int opcion = 0;
         int opcion2 = 0;
 
@@ -21,50 +20,48 @@ public class CocheMain {
 
             switch (opcion) {
                 case 1:
-                        meterDatos(sc);
+                    meterDatos(sc);
 
-                        do {
-                            System.out.println("Hola"+getnombre()+" elige una opcion: ");
-                            System.out.println("1. Ver coches disponibles");
-                            System.out.println("2. Configurar un coche nuevo");
-                            System.out.println("3. Ver factura");
-                            System.out.println("4. Salir");
-                            opcion2 = sc.nextInt();
-            
-                            switch (opcion2) {
-                                case 1:
-                                    verCoches();
-                                    break;
-                                case 2:
-                                    meterDatosCoche(sc);
-                                    break;
-                                case 3:
-                                    datosVenta();
-                                    break;
-                                default:
-                                    System.out.println("Opcion no valida");
-                                    break;
-                            }
-            
-                        } while (opcion2 != 4);
+                    do {
+                        System.out.println("Hola" + getnombre() + " elige una opcion: ");
+                        System.out.println("1. Ver coches disponibles");
+                        System.out.println("2. Configurar un coche nuevo");
+                        System.out.println("3. Ver factura");
+                        System.out.println("4. Salir");
+                        opcion2 = sc.nextInt();
+
+                        switch (opcion2) {
+                            case 1:
+                                verCoches();
+                                break;
+                            case 2:
+                                meterDatosCoche(sc);
+                                break;
+                            case 3:
+                                datosVenta();
+                                break;
+                            default:
+                                System.out.println("Opcion no valida");
+                                break;
+                        }
+
+                    } while (opcion2 != 4);
 
                     break;
-            
+
                 default:
-                        System.out.println("Opcion invalida");
+                    System.out.println("Opcion invalida");
                     break;
             }
 
         } while (opcion != 2);
 
-        
     }
 
-    public static void verCoches(){
-
+    public static void verCoches() {
         Coche coche1 = new Coche(2222, "Ferrari", "488 Pista", "Deportivo", null, null);
         Coche coche2 = new Coche(3332, "Toyota", "Yaris", "Compacto", null, null);
-        
+
         System.out.println("Estos son los coches que tenemos en stock");
         System.out.println(coche1);
         System.out.println(coche2);
