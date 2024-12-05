f512ea09bce3a84def17f3d7baa159b24fcf3940
Abdelilah Hajji <abdehajj80@gmail.com>
2024-12-05 17:34:03 +0100

    Modificada la clase Cliente

--- Coche.java
+++ Coche.java
@@ -1,4 +1,11 @@
 public class Coche {
     
-    
+    private String marca;
+    private String modelo;
+    private String matricula;
+    private double precio;
+
+    protected void meterDatos(){
+        
+    }
 }
