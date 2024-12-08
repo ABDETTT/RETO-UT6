ac805931f67cebfb50f9be9b33194eb4d4109266
Amine55ee <marruecosrabat4@gmail.com>
2024-12-08 20:18:51 +0100

    Act

--- Coche.java
+++ Coche.java
@@ -8,8 +8,8 @@ public class Coche {
     private String modelo; // Modelo del coche(Ibiza, Corolla, Leon...).
     private String categoria; // Categoria del coche(Súv, Coupe, Todoterreno...).
     private CaracteristicasCoche caracteristicas; // Caracteristicas del coche(Color, Motorización, Transmision...).
-    
-    public static double precioCoche=0;
+
+    public static double precioCoche = 0;
 
     public Coche(int id, String marca, String modelo, String categoria, CaracteristicasCoche caracteristicas,
             ExtrasCoche extras) {
@@ -20,109 +20,75 @@ public class Coche {
         this.caracteristicas = caracteristicas;
     }
 
-    
-
     public Scanner getSc() {
         return sc;
     }
-
-
 
     public void setSc(Scanner sc) {
         this.sc = sc;
     }
 
-
-
     public int getId() {
         return id;
     }
-
-
 
     public void setId(int id) {
         this.id = id;
     }
 
-
-
     public String getMarca() {
         return marca;
     }
-
-
 
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
-
-
     public String getModelo() {
         return modelo;
     }
-
-
 
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-
-
     public String getCategoria() {
         return categoria;
     }
-
-
 
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
-
-
     public CaracteristicasCoche getCaracteristicas() {
         return caracteristicas;
     }
-
-
 
     public void setCaracteristicas(CaracteristicasCoche caracteristicas) {
         this.caracteristicas = caracteristicas;
     }
 
-
-
     public ExtrasCoche getExtras() {
         return extras;
     }
-
-
 
     public void setExtras(ExtrasCoche extras) {
         this.extras = extras;
     }
 
-
-
     public static double getPrecioCoche() {
         return precioCoche;
     }
-
-
 
     public static void setPrecioCoche(double precioCoche) {
         Coche.precioCoche = precioCoche;
     }
 
-
-
-    protected void configurarCoche(){
-        precioCoche =+ caracteristicas.getprecioCocheCarac();
+    protected void configurarCoche() {
+        precioCoche = +caracteristicas.getprecioCocheCarac();
     }
 
-    private void meterDatosCoche(){
+    private void meterDatosCoche() {
 
         System.out.println("Introduce la Marca que quieras(Toyota, Ferrai, Porsche )");
         marca = sc.nextLine();
@@ -133,7 +99,8 @@ public class Coche {
         solicitarCaracterísticas();
     }
 
-    protected void resumenCoche(){
-        System.out.println("\nResumen del coche configurado: Id: "+id+"Marca: "+marca+"Modelo: "+modelo+"Categoria: "+categoria+"Características: "+caracteristicas+"Precio total: "+precioCoche);
+    protected void resumenCoche() {
+        System.out.println("\nResumen del coche configurado: Id: " + id + "Marca: " + marca + "Modelo: " + modelo
+                + "Categoria: " + categoria + "Características: " + caracteristicas + "Precio total: " + precioCoche);
     }
 }
